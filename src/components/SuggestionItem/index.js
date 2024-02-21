bfb4d72569c3a32a601b9cb0d9abdbb4439a1af5
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-02-21 05:56:08 +0000

    first commit

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {key, suggestion, suggestionClickResult} = props
+
+  const clickArrowSuggestion = () => {
+    suggestionClickResult(suggestion)
+  }
+
+  return (
+    <li className="suggestion-container">
+      <p className="item">{suggestion}</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        className="arrow-sign"
+        alt="arrow"
+        onClick={clickArrowSuggestion}
+      />
+    </li>
+  )
+}
+
+export default SuggestionItem
